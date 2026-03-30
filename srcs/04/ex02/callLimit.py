fc0585d180104febed13f95810181d82e36d3c93
mleproux <marcolivierleprouxpro@gmail.com>
2026-03-30 15:27:32 +0200

    [03] Finished polishing module 04

--- srcs/04/ex02/callLimit.py
+++ srcs/04/ex02/callLimit.py
@@ -1,11 +1,14 @@
 def callLimit(limit: int):
+    """Call a function or block the call if the limit threshold was reached"""
     count = 0
+
     def callLimiter(function):
+
         def limit_function(*args: any, **kwds: any):
             nonlocal count
             if (count >= limit):
                 print(f"Error: {function} call too many times")
-                return 
+                return
             count += 1
             function()
         return limit_function
--- srcs/04/ex03/new_student.py
+++ srcs/04/ex03/new_student.py
@@ -1,20 +1,28 @@
 import random
 import string
-
-
 from dataclasses import dataclass, field
+
+
 def generate_id() -> str:
-    return "".join(random.choices(string.ascii_lowercase, k = 15))
+    """Generate a random id of 15 lowercased characters"""
+    return "".join(random.choices(string.ascii_lowercase, k=15))
 
 
 @dataclass
 class Student:
+    """Student informations including :
+    - Name
+    - Surname
+    - Is actice (true by default)
+    - A randomly generated id
+    - A login using the first letter
+    of the name concatened with the username"""
     name: str
     surname: str
     active: bool = field(default=True)
     id: str = field(init=False)
     login: str = field(init=False)
-    
+
     def __post_init__(self):
         self.id = generate_id()
         self.login = f"{self.name[0]}{self.surname}"
